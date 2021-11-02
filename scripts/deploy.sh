456eec422dd12b62c4a9166beaaeb32c54d243fa
Neil Wang <wangweili457@gmail.com>
2021-11-02 16:21:05 +0800

    DONE: CI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-ssh -i ~/.ssh/id_rsa root@47.102.40.210 "docker rm -f neilwang.wiki"
-ssh -i ~/.ssh/id_rsa root@47.102.40.210 "docker pull 47.102.40.210:5000/neilwang.wiki:latest"
-ssh -i ~/.ssh/id_rsa root@47.102.40.210 "docker run --name neilwang.wiki -d -p 10080:80 --restart always 47.102.40.210:5000/neilwang.wiki:latest"
+ssh -i ~/.ssh/id_rsa root@217.70.191.179 "docker rm -f neilwang.wiki"
+ssh -i ~/.ssh/id_rsa root@217.70.191.179 "docker pull 47.102.40.210:5000/neilwang.wiki:latest"
+ssh -i ~/.ssh/id_rsa root@217.70.191.179 "docker run --name neilwang.wiki -d -p 10080:80 --restart always 47.102.40.210:5000/neilwang.wiki:latest"
